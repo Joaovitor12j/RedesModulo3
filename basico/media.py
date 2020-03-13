cb73ee7f2109bd9fbbc25e840cc4ebe7d4c2b94e
José Dihego <josedihego@gmail.com>
2020-03-12 21:14:33 -0300

    algoritmo média

--- basico/media.py
+++ basico/media.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+#print 28/5
+#print 28/5.0
+
 L = []
 
 print 'Informe os valores a serem inseridos na lista' \
@@ -7,11 +10,17 @@ print 'Informe os valores a serem inseridos na lista' \
 
 valor_digitado = 'qualquer coisa'
 
-while valor_digitado != 'SAIR':
+while valor_digitado.lower() != 'sair':
     valor_digitado = raw_input()
-    if(valor_digitado != 'SAIR'):
-        L.append(valor_digitado)
+    if(valor_digitado.lower() != 'sair'
+            and valor_digitado.isdigit()):
+        L.append(int(valor_digitado))
+    else:
+        print 'opção inválida'
+
+
 
 print L
 
+print 'Média:', sum(L)/float(len(L))
 
